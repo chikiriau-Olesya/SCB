82ad56bf50c6d033ba7e3e283f7c7e5e748bae0f
chikiriau-Olesya <faceremirabilia@gmail.com>
2020-04-07 23:56:06 +0300

    edit js

--- js/main.js
+++ js/main.js
@@ -1,62 +1,39 @@
-// const btn = document.querySelector('body')
-// btn.onclick = () => {
-//     let div = document.createElement('div')
-//     div.classList.toggle('gravity')
-//     div.style.width = '200px'
-//     div.style.height = '200px'
-//     div.style.backgroundColor = 'red'
-//     div.style.zIndex = 4
-//     document.body.append(div)
-// }
 
-
-// const btn = document.querySelector('body')
-
-// btn.onclick = () => {
-//     const numberOfNew = 10
+// const numberOfNew = 10
 
 //     if (numberOfNew < 11) {
 //         let div = document.createElement('div')
+//         div.style.width = '200px'
+//         div.style.height = '200px'
+//         div.style.position = 'absolute'
+//         div.style.top = 0
+//         div.style.zIndex = 3
+//         div.style.left = '45%'
+//         div.style.backgroundColor = 'blue'
     
 //         let animeDiv = anime({
 //             targets: div,
-//             translateY: 500,
-//             delay: 3,
-//             duration: 200,
+//             translateY: 300,
+//             delay: 60,
+//             duration: 4000,
 //             easing: 'linear'
     
 //         })
     
-//         div.style.width = '200px'
-//         div.style.height = '200px'
-//         div.style.backgroundColor = 'blue'
 //         div.classList.toggle('gravity') 
+//         document.body.append(div)
 //     }
-//  }
 
-const numberOfNew = 10
-
-    if (numberOfNew < 11) {
-        let div = document.createElement('div')
-        div.style.width = '200px'
-        div.style.height = '200px'
-        div.style.position = 'absolute'
-        div.style.top = 0
-        div.style.zIndex = 3
-        div.style.left = '45%'
-        div.style.backgroundColor = 'blue'
-    
-        let animeDiv = anime({
-            targets: div,
-            translateY: 300,
-            delay: 60,
-            duration: 4000,
-            easing: 'linear'
-    
-        })
-    
-        div.classList.toggle('gravity') 
-        document.body.append(div)
+const btn = document.querySelector('body')
+btn.onlick = function () {
+    let div = document.querySelectorAll('hidden')
+    for (let i = 0; i < div.length; i++) {
+        const element = div[i];
+        element.style.top = '0'
+        element.style.visibility = 'visible'
+        
     }
+   
+}
 
 
