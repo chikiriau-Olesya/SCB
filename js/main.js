0548d90931b2a2119150a4239120016c1af19a78
chikiriau-Olesya <faceremirabilia@gmail.com>
2020-04-09 09:47:40 +0300

    add

--- js/main.js
+++ js/main.js
@@ -1,39 +1,44 @@
-
-// const numberOfNew = 10
-
-//     if (numberOfNew < 11) {
-//         let div = document.createElement('div')
-//         div.style.width = '200px'
-//         div.style.height = '200px'
-//         div.style.position = 'absolute'
-//         div.style.top = 0
-//         div.style.zIndex = 3
-//         div.style.left = '45%'
-//         div.style.backgroundColor = 'blue'
-    
-//         let animeDiv = anime({
-//             targets: div,
-//             translateY: 300,
-//             delay: 60,
-//             duration: 4000,
-//             easing: 'linear'
-    
-//         })
-    
-//         div.classList.toggle('gravity') 
-//         document.body.append(div)
-//     }
-
-const btn = document.querySelector('body')
-btn.onlick = function () {
-    let div = document.querySelectorAll('hidden')
-    for (let i = 0; i < div.length; i++) {
-        const element = div[i];
-        element.style.top = '0'
-        element.style.visibility = 'visible'
-        
-    }
-   
+// Change drag and drop cursor
+function changeCursor (el) {
+    el.onmousedown = function (e) {
+        el.style.cursor = 'grabbing'
+     }
+    el.onmouseup = function (e) {
+        el.style.cursor = 'grab'
+     }
 }
+
+let metal= document.querySelector('.metal')
+changeCursor(metal)
+
+let can= document.querySelector('.can')
+changeCursor(can)
+
+let paper= document.querySelector('.paper')
+changeCursor(paper)
+
+let smallPaper= document.querySelector('.small-paper')
+changeCursor(smallPaper)
+
+let banana= document.querySelector('.banana')
+changeCursor(banana)
+
+
+let animation = anime ({
+ targets: '.new10',
+ autoplay: true,
+ translateX: ['26vw', '26vw'],
+ translateY: ['-150vw', '20vw'],
+ direction: 'linear',
+ duration: 3000
+})
+
+
+
+
+
+
+
+
 
 
